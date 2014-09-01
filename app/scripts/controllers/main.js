c68ed55d21a4a113f7b0ae7b9a14481c64f74f5e
ymmn <abdnimery92@gmail.com>
2014-08-31 19:16:32 -0700

    scaffold by yeoman

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('guessTheSongApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
